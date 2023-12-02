35d7e3bf745f407b42e12b2ec163ffcac36585cc
Jonathan <jhyahav2@gmail.com>
2023-12-02 12:14:40 +0200

    replace stale img links

--- src/mockData.ts
+++ src/mockData.ts
@@ -5,10 +5,10 @@ const mockData: ListingCardData[] = [
     id: 1,
     type: "Sublet",
     imageUrls: [
-      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/382004705_10227052969584051_3585491216740764864_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=vqWGhCxRD3wAX8Nadqi&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfAbP1yn4rgvIwj1kvwPqkv7Kr9_0W1o93Qj4A88pchN2Q&oe=651CC1CA",
-      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/381700565_10227052969824057_7759663276205505855_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=NT24dlLOMB4AX-JCQtj&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfD21UHp7--1cS8AhJM9-5TrE_yYs8JleHQBGvHdCSIJgg&oe=651E0247",
-      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/380114736_10227052969384046_7120677605679444015_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=g_DPgzY7QfAAX90A4My&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfAyOJaSPOBqiQJ8-0u9bSFrmQtIn2a7vDleW-PbPdlwpw&oe=651CF183",
-      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/382041462_10227052969504049_5838162550514601571_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=AJQpg0QEytUAX-7fdCx&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfCZtkBKaGhbaM7xYz_zk-iuv_Xx2F6XNDyvL2cRiBrXBA&oe=651CE9A3",
+      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/407249610_6306268542808178_7123413893417789144_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=c42490&_nc_ohc=U15OGcrgfAIAX_kbmO_&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfBPm-mM2863yASq4mCuF8amG3lsVMOyPcBpLaHaVNjXPA&oe=656F01F0",
+      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/407279861_6306268642808168_1041550016448075672_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=c42490&_nc_ohc=Iz7QXEQCbRQAX-ISWm9&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfBUoFUOxkJ6tuN5drmb5nMC7kU2nMqVSZqPn1s5YzWlJA&oe=65707125",
+      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/407365158_6306267782808254_2655054942379401918_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=c42490&_nc_ohc=gOfq0yILamcAX-ifCqY&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfBwKPyN-qHO9q6uma2For4oYGp5mc-geh22LO4oXAdTKw&oe=656F5976",
+      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/407297226_6306269259474773_8774877539724318012_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=c42490&_nc_ohc=fGwpgrjjrh4AX-udLqJ&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfDAMGYeFsrKSCLq9VyskBEmJiV2RfqVPMqm411ec9QH-Q&oe=656FAA77",
     ],
     city: "Tel Aviv",
     postDate: "2023-09-30",
@@ -22,7 +22,7 @@ const mockData: ListingCardData[] = [
     id: 3,
     type: "Sublet",
     imageUrls: [
-      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/385701602_7447298368620850_859536817781652954_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=4c1e7d&_nc_ohc=OSG__zbFQEIAX_78JJD&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfCPNDTCY1DQZpABMtx3ZdIeOeGN3aAGcjfm0mxMVHNygQ&oe=651CAEB0",
+      "https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/407417040_1527027091380505_6514489523390287684_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=c42490&_nc_ohc=gpyiAX1ZnBsAX-BNACb&_nc_ht=scontent.ftlv5-1.fna&oh=00_AfDySonhQp_fiK8xCiNnGAMM0Lt2LZXyIg59eCKKea-0Qg&oe=6570561F",
     ],
     city: "Tel Aviv",
     postDate: "2023-09-30",
