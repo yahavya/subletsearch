4408e0a06e68788c48d87dfd207595f18e7b66cb
Jonathan <jhyahav2@gmail.com>
2023-12-02 16:32:18 +0200

    create listing card subcomponents

---
+++ src/components/LocationRow.tsx
@@ -0,0 +1,29 @@
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
+import IconField from "./IconField"
+import { ListingCardData } from "../types"
+import { FC } from "react"
+import { Box, styled } from "@mui/material"
+
+const StyledContainer = styled(Box)({
+  display: "flex",
+  alignItems: "flex-start",
+})
+
+type Props = Pick<ListingCardData, "neighborhood" | "street">
+
+const LocationRow: FC<Props> = ({ neighborhood, street }) => {
+  if (!(street || neighborhood)) return null
+  const locationString = street
+    ? neighborhood
+      ? `${street}, ${neighborhood}`
+      : street
+    : neighborhood
+  return (
+    <StyledContainer>
+      <IconField icon={faLocationDot} typographyVariant="h4" iconSize="lg">
+        {locationString}
+      </IconField>
+    </StyledContainer>
+  )
+}
+export default LocationRow
