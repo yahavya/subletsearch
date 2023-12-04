1d34a818f41da468f1d1b5cbb6842cce70fbba01
Jonathan <jhyahav2@gmail.com>
2023-12-04 21:28:52 +0200

    move backend files

--- src/components/ListingDetails.tsx
+++ src/components/ListingDetails.tsx
@@ -1,8 +1,9 @@
-import { FC } from "react";
-import { ListingFullData } from "../types";
+import { FC } from "react"
+import { ListingFullData } from "../types"
+import { Typography } from "@mui/material"
 
-const ListingDetails: FC<ListingFullData> = (data) => {
-  return <div>{data.city}</div>;
-};
+const ListingDetails: FC<ListingFullData> = data => {
+  return <Typography variant="h1">{data.city}</Typography>
+}
 
-export default ListingDetails;
+export default ListingDetails
