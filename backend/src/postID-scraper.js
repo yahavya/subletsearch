dfc80b19aee4b818d079df4f636cbb94e56745ae
Ron <ronnieyahav@gmail.com>
2023-12-13 10:26:29 +0200

    small changes

--- backend/src/postID-scraper.js
+++ backend/src/postID-scraper.js
@@ -18,7 +18,7 @@ async function scraper() {
     });
 
     const page = await browser.newPage();
-    await page.goto('https://m.facebook.com/groups/447412252098033/');
+    await page.goto('https://m.facebook.com/groups/327655587294381/');
 
     const postElements = await page.$$('#m_group_stories_container > section [class="_55wo _5rgr _5gh8 async_like"]');
     await page.waitForSelector('#m_group_stories_container > section [class="_55wo _5rgr _5gh8 async_like"]', { timeout: 5000 });
@@ -26,7 +26,8 @@ async function scraper() {
     for (const postElement of postElements){ // IMPORTANT NOTE: Error handling isn't done well here, so must check to see if there are other errors manually (we catch element not found error and skip element)
         try { 
           const postText = await postElement.$eval('*', (el) => el.textContent);
-          const postUrl = await postElement.$eval('div._5rgt._5nk5._5wnf._5msi > a', (el) => el.href)
+          // need to understand if we should add support for elements with "_5rgt _5nk5 _5wnf _5msi" div
+          const postUrl = await postElement.$eval('div._5rgt._5nk5._5msi > a', (el) => el.href)
           const questionMarkIndex = postUrl.indexOf('?')
           const shortenedUrl = postUrl.substring(0, questionMarkIndex);
           const MobileToWebUrl = shortenedUrl.replace('https://m.facebook.com/', 'https://www.facebook.com/')
@@ -34,12 +35,12 @@ async function scraper() {
             text: postText,
             url: MobileToWebUrl
           });
-          console.log(list)
       }
       catch (error){
         console.log("this post doesn't have a link: ")
       }
     }
+    console.log(list)
     console.log(list.length)
 
 
