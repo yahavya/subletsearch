3ee484f32b33b0fb0645611f0e7e2d6485066dce
Ron <ronnieyahav@gmail.com>
2023-12-16 16:04:35 +0200

    scrape first image off every post, basic error handling

--- backend/src/image-scraper.js
+++ backend/src/image-scraper.js
@@ -25,40 +25,35 @@ async function scraper() {
     const imageElements = await page.$$('#m_group_stories_container > section [class="_5rgu _7dc9 _27x0"]')
     
     // image types: _50xr _403j AND ._5sgk.403j
-
-    const count = 1
-
+    let count = 0;
     for (const postElement of postElements){ // IMPORTANT NOTE: Error handling isn't done well here, so must check to see if there are other errors manually (we catch element not found error and skip element)
-        
           const postText = await postElement.$eval('*', (el) => el.textContent);
           const currentImageList = []
           const ImageUrlList = []
-          // need to understand if we should add support for elements with "_5rgt _5nk5 _5wnf _5msi" div
+          
+          try {
           const postUrl = await postElement.$eval('div._5rgt._5nk5._5msi > a', (el) => el.href)
-          //const image2 = await postElement.$eval('div._5sgk._403j > img', (img) => img.src)
           const questionMarkIndex = postUrl.indexOf('?')
           const shortenedUrl = postUrl.substring(0, questionMarkIndex);
           const MobileToWebUrl = shortenedUrl.replace('https://m.facebook.com/', 'https://www.facebook.com/')
-          //for (const ... ) {} // fill this in
-            const imageUrl = await postElement.$eval('div._5sgk._403j > img, div._50xr._403j > img', (img) => img.src) //currently we're getting one image from each listing and not all images from each listing
-          
-          console.log("THIS IS IMAGE URL: " + imageUrl)
-          console.log(count)
-          count = count + 1
-          //console.log("AND THIS IS URL: " + MobileToWebUrl)
-          ImageUrlList.push(imageUrl)
 
+          //for (const ... ) {} // fill this in
+          const imageUrl = await postElement.$eval('div._5sgk._403j > img, div._50xr._403j > img', (img) => img.src) //currently we're getting one image from each listing and not all images from each listing
+
+          ImageUrlList.push(imageUrl)
           list.push({
             text: postText,
             url: MobileToWebUrl,
             images: ImageUrlList
     })
+} catch (error) {
+    count +=1
+}
     }
-    console.log(list)
-    console.log(list.length)
-
-
+    console.log("errors in total of " + count + " posts")
+    
     await browser.close();
+
   } catch (error) {
     console.error('An error occurred:', error);
   }
