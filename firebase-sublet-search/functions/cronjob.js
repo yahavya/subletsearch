868595f08073bb4a87bbca3ab5d67bb08b504fa6
Ron <ronnieyahav@gmail.com>
2024-06-01 16:36:43 +0300

    added potential help for building cronjob

--- firebase-sublet-search/functions/cronjob.js
+++ firebase-sublet-search/functions/cronjob.js
@@ -1,3 +1,6 @@
 /* 1. Need to figure out how to run the scraper on a schedule.
 2. Also need to figure out how to make post requests with firebase functions.
+Look here for further guidance: 
+
+https://stackoverflow.com/questions/47659525/creating-a-document-in-firestore-using-cloud-functions
 */
