4bc8b4658dfb59922a4ea1cc5bf922cbae7a9218
Ron <ronnieyahav@gmail.com>
2024-06-30 23:08:30 +0300

    fun on FE - add slider, add typing animation

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,14 +1,15 @@
 import ListingCard from "../components/ListingCard"
 //import mockData from "../mockData"
 import React, { useEffect, useState } from "react"
-
+import Slider from "@mui/material/Slider";
+import Typography from "@mui/material/Typography";
 import {TailSpin} from "react-loader-spinner";
 
 interface Listing {
   id: number;
   postUrl: string;
   imageUrl: string[];
-  price: number | string;
+  price: number;
   entry_date: string
   startDate: string;
   endDate: string;
@@ -29,8 +30,7 @@ interface ApiResponse {
 
 const FeedPage = () => {
   const [data, setData] = useState<ApiResponse | null>(null);
-  
-
+  const [priceRange, setPriceRange] = useState<[number, number]>([0, 10000]);
 
   useEffect(() => {
     fetch('https://us-central1-sublet-search-8ae44.cloudfunctions.net/app/api/listings')
@@ -40,46 +40,56 @@ const FeedPage = () => {
   }, []);
 
   return (
+    <><div style={{ marginBottom: "20px", display: "flex", justifyContent: "center"  }}>
+      <Typography variant="h6" gutterBottom style={{marginRight: "20px"}}>Adjust Price</Typography>
+      <Slider
+        value={priceRange}
+        onChange={(event, newValue) => setPriceRange(newValue as [number, number])}
+        valueLabelDisplay="auto"
+        min={1000}
+        max={10000}
+        step={50}
+        style={{ width: "30%"}} />
+    </div>
     <div style={{ display: "flex", flexWrap: "wrap", gap: 16, marginTop: 10 }}>
-      {data ? (
-        data.listings.map((listing, index) => {
-          // Check if the listing has enough data
-        if (
-          listing.postUrl &&
-          listing.imageUrl &&
-          typeof listing.price === 'number' && listing.price > 1000          
-        ) {
-          return (
-            <div key={index} style={{ width: "calc(25% - 16px)" }}>
-              <ListingCard
-                postUrl={listing.postUrl}
-                id={listing.id}
-                imageUrls={listing.imageUrl}
-                price={listing.price ? listing.price : "מחיר בפרטי"}
-                postDate={listing.entry_date}
-                startDate={listing.startDate}
-                endDate={listing.endDate}
-                neighborhood={listing.neighborhood || ""}
-                street={listing.street || ""}
-                roomCount={listing.roomCount}
-                floorNumber={listing.floorNumber || 0}
-                area={listing.area || 0}
-                city="Tel Aviv"
-              />
-            </div>
-          );
-        }
-        return null; // Skip rendering if the listing has mostly null values
-      })
-      ) : (
-        <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+        {data ? (
+          data.listings
+            .filter((listing) => listing.price >= priceRange[0] && listing.price <= priceRange[1])
+            .map((listing, index) => {
+              // Check if the listing has enough data
+              if (listing.postUrl &&
+                listing.imageUrl &&
+                typeof listing.price === 'number' && listing.price > 1000) {
+                return (
+                  <div key={index} style={{ width: "calc(25% - 16px)" }}>
+                    <ListingCard
+                      postUrl={listing.postUrl}
+                      id={listing.id}
+                      imageUrls={listing.imageUrl}
+                      price={listing.price}
+                      postDate={listing.entry_date}
+                      startDate={listing.startDate}
+                      endDate={listing.endDate}
+                      neighborhood={listing.neighborhood || ""}
+                      street={listing.street || ""}
+                      roomCount={listing.roomCount}
+                      floorNumber={listing.floorNumber || 0}
+                      area={listing.area || 0}
+                      city="Tel Aviv" />
+                  </div>
+                );
+              }
+              return null; // Skip rendering if the listing has mostly null values
+            })
+        ) : (
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
             <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
               <TailSpin color="#00BFFF" height={80} width={80} />
             </div>
-        </div> 
+          </div>
 
-      )}
-    </div>
+        )}
+      </div></>
   );
 };
 
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,25 @@
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { Link } from'react-router-dom';
+import { ReactTyped } from 'react-typed';
+import { useEffect, useRef } from 'react'
 
 const LandingPage = () => {
   return (
+    <><div><ReactTyped strings={["Welcome to sublet.search. I'm Yaron, and this site was made in order to help you find your next sublet. Click on the button below to get started!"]} typeSpeed={40} 
+    style={{
+    justifyContent: "center",
+    display: "flex",
+    fontSize: "2rem", // Change the font size to 2rem (adjust as needed)
+    fontFamily: "Roboto, sans-serif", // Change the font to Roboto or Calibri
+    textAlign: "center"
+  }}></ReactTyped>
+    </div>
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-     <Link to="/browse"> 
-      <Button variant="contained" color="primary">Press here to see listings</Button>
+     <Link to="/browse">
+      <Button variant="contained" color="primary" style={{fontSize: "1.5rem"}}>Press here to see listings</Button>
     </Link>
     </div>
+    </>
   );
 };
 
